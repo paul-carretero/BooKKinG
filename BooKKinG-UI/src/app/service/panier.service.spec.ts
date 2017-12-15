c6402c2167de039889f38b4616d59304345e0f1d
Myriam Clouet <clouet.m@laposte.net>
2017-12-15 20:49:07 +0100

    ajout de tests pour panierService

--- BooKKinG-UI/src/app/service/panier.service.spec.ts
+++ BooKKinG-UI/src/app/service/panier.service.spec.ts
@@ -1,3 +1,5 @@
+import { Article } from './../model/article';
+import { Livre } from './../model/livre';
 import { AchatService } from './achat.service';
 import { ConnectionService } from './connection.service';
 import { TestBed, inject } from '@angular/core/testing';
@@ -24,4 +26,118 @@ describe('PanierService', () => {
   it('should be created', inject([PanierService], (service: PanierService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('ajouter un livre dans le panier', inject([PanierService], (service: PanierService) => {
+    let livre : Livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:1, price:5.6,
+      type:"type", stock:10
+    }
+    let quantity = 1;
+    service.ajouterLivrePanier(livre,quantity);
+    let contenuPanier : Article[] = service.getContenuPanier();
+    let contenuAttendu : Article[] =[{book:livre, quantity:quantity, idBook:livre.idBook}]
+    expect(contenuPanier).toEqual(contenuAttendu);
+  }));
+
+
+  it('erreur ajouter un livre dans le panier', inject([PanierService], (service: PanierService) => {
+    let livre : Livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:1, price:5.6,
+      type:"type", stock:10
+    }
+    let quantity = 1;
+    service.ajouterLivrePanier(livre,quantity);
+    let contenuPanier : Article[] = service.getContenuPanier();
+    let contenuPasAttendu : Article[] =[{book:livre, quantity:quantity+2, idBook:livre.idBook}]
+    expect(contenuPanier).not.toEqual(contenuPasAttendu);
+  }));
+
+  it('calcul du prix total dans le panier', inject([PanierService], (service : PanierService) =>{
+    let livre : Livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:1, price:5,
+      type:"type", stock:10
+    }
+    let quantity = 2;
+    service.ajouterLivrePanier(livre,quantity);
+    livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:2, price:3,
+      type:"type", stock:10
+    }
+    quantity = 1;
+    service.ajouterLivrePanier(livre,quantity);
+    let prixTotal = service.getTotalPrice();
+    let prixAttendu = 13;
+    expect(prixTotal).toEqual(prixAttendu);
+  }));
+
+  it('erreur calcul du prix total dans le panier', inject([PanierService], (service : PanierService) =>{
+    let livre : Livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:1, price:5,
+      type:"type", stock:10
+    }
+    let quantity = 2;
+    service.ajouterLivrePanier(livre,quantity);
+    livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:2, price:3,
+      type:"type", stock:10
+    }
+    quantity = 1;
+    service.ajouterLivrePanier(livre,quantity);
+    let prixTotal = service.getTotalPrice();
+    let prixPasAttendu = 20;
+    expect(prixTotal).not.toEqual(prixPasAttendu);
+  }));
+
+  it('nombre de livre dans le panier', inject([PanierService], (service : PanierService) =>{
+    let livre : Livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:1, price:5,
+      type:"type", stock:10
+    }
+    let quantity = 2;
+    service.ajouterLivrePanier(livre,quantity);
+    livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:2, price:3,
+      type:"type", stock:10
+    }
+    quantity = 1;
+    service.ajouterLivrePanier(livre,quantity);
+    let nbLivre = service.getNumberOfItems();
+    let nbLivreAttendu = 3;
+    expect(nbLivre).toEqual(nbLivreAttendu);
+  }));
+
+
+
+  it('mise a jour quantite livre dans panier', inject([PanierService], (service : PanierService) =>{
+    let livre : Livre = {
+      title:"titre", author:"auteur", 
+      genre:"genre", summary:"résumé", 
+      idBook:1, price:5,
+      type:"type", stock:10
+    }
+    let quantity = 2;
+    service.ajouterLivrePanier(livre,quantity);
+    quantity = 5;
+    service.setQuantity(livre.idBook,quantity);
+    let quantityLivre = service.getContenuPanier()[0].quantity;
+    let quantityAttendue = 5;
+    expect(quantityLivre).toEqual(quantityAttendue);
+  }));
+
 });
