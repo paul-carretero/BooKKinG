00cb4d30ed9aeca09437f80c874573c35fe03bc8
tiretfa <tiretfabien@hotmail.fr>
2017-12-18 12:07:58 +0100

    commit update connectionService spec to pull

--- BooKKinG-UI/src/app/service/connection.service.spec.ts
+++ BooKKinG-UI/src/app/service/connection.service.spec.ts
@@ -4,6 +4,7 @@ import { ConnectionService } from './connection.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule } from '@angular/http';
 import { NotifService } from './notif.service';
+import { Client } from './../model/client';
 
 describe('ConnectionService', () => {
   beforeEach(() => {
@@ -22,4 +23,32 @@ describe('ConnectionService', () => {
   it('should be created', inject([ConnectionService], (service: ConnectionService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get status', inject([ConnectionService], (service : ConnectionService) =>
+  {
+    const client : Client = {
+      name : 'testUser',
+      address : 'testAddress',
+      email: 'testEmailAddress',
+      password: 'testpassword',
+      admin : false
+    }
+    service.connection(client);
+    expect(service.getConnectionStatus());
+  }));
+
+  it('should get client ', inject([ConnectionService], (service : ConnectionService) =>
+  {
+    const client : Client = {
+      name : 'testUser',
+      address : 'testAddress',
+      email: 'testEmailAddress',
+      password: 'testpassword',
+      admin : false
+    }
+    service.connection(client);
+    let clientGetted = service.getCurrentUser();
+    expect(clientGetted).toEqual(client);
+  }));
+
 });
