d1916f1b2b89c483397ec5c5d0bb14031ee04b9f
tiretfa <tiretfabien@hotmail.fr>
2017-12-18 10:55:26 +0100

    test spec pour filtre et connection component

--- BooKKinG-UI/src/app/component/filtre/filtre.component.spec.ts
+++ BooKKinG-UI/src/app/component/filtre/filtre.component.spec.ts
@@ -9,6 +9,11 @@ import { FiltreComponent } from './filtre.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule } from '@angular/http';
 import { InitService } from '../../service/init.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NotifService } from '../../service/notif.service';
+import { AchatService } from '../../service/achat.service';
+import { PanierService } from '../../service/panier.service';
+import { ConnectionService } from '../../service/connection.service';
 
 describe('FiltreComponent', () => {
   let component: FiltreComponent;
@@ -18,11 +23,17 @@ describe('FiltreComponent', () => {
     TestBed.configureTestingModule({
       declarations: [FiltreComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA],
       imports: [
+
         RouterTestingModule,
         HttpModule
       ],
       providers: [
+        NotifService,
+        AchatService,
+        PanierService,
+        ConnectionService,
         LivreService,
         LRUCacheService,
         NavigationService,
