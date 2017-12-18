94c5f200f0b661ae17adbe36347f655e41f46fe1
minchunnguyen <minchun_nguyen@yahoo.com>
2017-12-18 14:58:09 +0100

    test historique-page + init service

--- BooKKinG-UI/src/app/service/historique-pages.service.spec.ts
+++ BooKKinG-UI/src/app/service/historique-pages.service.spec.ts
@@ -4,6 +4,8 @@ import { NavigationService } from './navigation.service';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { HistoriquePagesService } from './historique-pages.service';
+import { NavigationData } from '../model/navigation-data';
+import { Globals } from '../globals';
 
 describe('HistoriquePagesService', () => {
   beforeEach(() => {
@@ -19,5 +21,27 @@ describe('HistoriquePagesService', () => {
 
   it('should be created', inject([HistoriquePagesService], (service: HistoriquePagesService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('possible de retourner', inject([HistoriquePagesService,NavigationService], (service: HistoriquePagesService, serviceNav:NavigationService) => {
+    serviceNav.setCurrentPage(2);
+    serviceNav.setCurrentPage(3);
+    const retourner = service.canGoBack();
+    const retourner_valide = true;
+    expect(retourner).toEqual(retourner_valide);
+  }));
+
+  it('impossible de retourner', inject([HistoriquePagesService,NavigationService], (service: HistoriquePagesService, serviceNav:NavigationService) => {
+    const retourner = service.canGoBack();
+    const retourner_valide = false;
+    expect(retourner).toEqual(retourner_valide);
+  }));
+
+  it('naviguer page précédente', inject([HistoriquePagesService,NavigationService], (service: HistoriquePagesService, serviceNav:NavigationService) => {
+    serviceNav.setCurrentPage(2);
+    serviceNav.setCurrentPage(3);
+    let page_courante = service.navPagePrecedente();
+    expect(page_courante.nPage).toEqual(2);
+
   }));
 });
