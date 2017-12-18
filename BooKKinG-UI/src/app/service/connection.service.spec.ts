44f0e3a7adb0a911a7deb1d6406d8e216806009e
tiretfa <tiretfabien@hotmail.fr>
2017-12-18 12:41:10 +0100

    commit to update connection spec ts to pull

--- BooKKinG-UI/src/app/service/connection.service.spec.ts
+++ BooKKinG-UI/src/app/service/connection.service.spec.ts
@@ -33,8 +33,9 @@ describe('ConnectionService', () => {
       password: 'testpassword',
       admin : false
     }
-    service.connection(client);
-    expect(service.getConnectionStatus());
+    service.connection(client).subscribe(c =>{
+      expect(this.getConnectionStatus()).toEqual(true);
+    });    
   }));
 
   it('should get client ', inject([ConnectionService], (service : ConnectionService) =>
@@ -46,9 +47,55 @@ describe('ConnectionService', () => {
       password: 'testpassword',
       admin : false
     }
-    service.connection(client);
-    let clientGetted = service.getCurrentUser();
-    expect(clientGetted).toEqual(client);
+    service.connection(client).subscribe( c => {
+      let clientGetted = service.getCurrentUser();
+      expect(clientGetted).toEqual(client);
+    });
   }));
+    
+  it('should be disonnected', inject([ConnectionService], (service : ConnectionService) =>
+  {
+    const client : Client = {
+      name : 'testUser',
+      address : 'testAddress',
+      email: 'testEmailAddress',
+      password: 'testpassword',
+      admin : false
+    }
+    service.connection(client).subscribe(c =>{
+      this.deconnexion();
+      expect(this.getConnectionStatus()).toEqual(false);
+    });
+  }));
+
+
+  it('should modify client', inject([ConnectionService], (service : ConnectionService) =>
+  {
+    const client : Client = {
+      name : 'testUser',
+      address : 'testAddress',
+      email: 'testEmailAddress',
+      password: 'testpassword',
+      admin : false
+    }
+    service.connection(client).subscribe(c =>{
+      let clientModify : Client = {
+        name : 'testModifyUser',
+        address : 'testModifyAddress',
+        email: 'testModifyEmailAddress',
+        password: 'testModifypassword',
+        admin : false
+      }
+      service.modifierClient(clientModify).subscribe( cm =>{
+        expect(service.getCurrentUser()).toEqual(clientModify);
+
+      });
+    });
+
+  }));
+
+    
+    
+  
 
 });
